14b8430e87ede96722fb9e6a3b5206e473ad9768
Richard Bendli <93144317+richardbendli@users.noreply.github.com>
2023-11-23 18:18:05 +0000

    update

---
+++ 20_break_statement/20_code.py
@@ -0,0 +1,20 @@
+#Break Statement
+
+#Break keyword is available to use within loops, to immediately getting out of a for loop execution.
+
+
+#We should use break keyword when we want to get out of the associated for loop, usually we will do it to check some condition in each iteration, and once we hit true, then we will use break.
+#This is useful to efficient our program, and to prevent from unnecessary executions.
+
+#Example:
+friends = ["Josh", "Ben", "John", "Michael", "Jim"]
+best_friend = "Josh"
+
+for friend in friends:
+    if friend == best_friend:
+        print(f"{best_friend} is inside the list!")
+        break
+    else:
+        print(f"This is {friend}")
+
+print("I am just a random line after the for loop")
