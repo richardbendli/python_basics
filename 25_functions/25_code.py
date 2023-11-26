7e3a54d7d71a58545d7c1bb74896745edf659c8b
Richard Bendli <93144317+richardbendli@users.noreply.github.com>
2023-11-26 07:52:43 +0000

    function

---
+++ 25_functions/25_code.py
@@ -0,0 +1,16 @@
+#Functions
+
+#A function is a block of code which only runs when it is called.
+
+
+#Weq can CREATE a function by using the format:
+# def <name>():
+# <name> being the name that you'd like to give to your functions
+def special_count():
+    for i in range(1, 11, 1):
+        print(f"Special count {i}")
+
+#This is how you CALL to a function
+special_count() #Call
+special_count() #Call
+special_count() #Call
