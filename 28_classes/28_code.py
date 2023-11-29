e992f887303fbbb9904b74c5ca2eaf3a69520ea5
Richard Bendli <93144317+richardbendli@users.noreply.github.com>
2023-11-29 18:32:52 +0000

    Classes

---
+++ 28_classes/28_code.py
@@ -0,0 +1,12 @@
+#Classes
+
+#We can create our own variable types, using Classes
+
+class CreditCard:
+    pass
+
+name = "Jim" #Creating an instance of a string
+my_card = CreditCard() #Creating an instance of a CreditCard
+
+print(type(name)) #will print <class 'str'>
+print(type(my_card)) #will print <class '__main__.CreditCard'
